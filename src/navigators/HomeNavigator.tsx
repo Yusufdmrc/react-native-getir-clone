5162e9d575d4988c920667061aebae8dbfd4e06b
yusuf <yusuffdemirci10@gmail.com>
2023-10-07 15:22:50 +0300

    CartScreen created

--- src/navigators/HomeNavigator.tsx
+++ src/navigators/HomeNavigator.tsx
@@ -2,9 +2,11 @@ import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "../screens/HomeScreen";
 import CategoryScreen from "../screens/CategoryScreen";
 import ProductDetailsScreen from "../screens/ProductDetailsScreen";
-import { Image, Text } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import CartScreen from "../screens/CartScreen";
+import { Dimensions, Image, Text, TouchableOpacity, View } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+
 import {
   getFocusedRouteNameFromRoute,
   useNavigation,
@@ -12,6 +14,8 @@ import {
 import React from "react";
 
 const Stack = createStackNavigator();
+
+const { width, height } = Dimensions.get("window");
 
 const MyStack = ({ navigation, route }) => {
   const tabHiddenRoutes = ["ProductDetails"];
@@ -54,6 +58,47 @@ const MyStack = ({ navigation, route }) => {
           headerTitle: () => (
             <Text style={{ color: "#fff", fontSize: 16 }}>Ürünler</Text>
           ),
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => navigation.navigate("CartScreen")}
+              style={{
+                width: width * 0.22,
+                height: 33,
+                backgroundColor: "#fff",
+                marginRight: width * 0.03,
+                borderRadius: 9,
+                flexDirection: "row",
+                alignItems: "center",
+              }}
+            >
+              <Image
+                style={{
+                  width: 23,
+                  height: 23,
+                  marginLeft: 6,
+                }}
+                source={require("../../assets/cart.png")}
+              />
+              <View
+                style={{
+                  flex: 1,
+                  height: 33,
+                  borderTopRightRadius: 9,
+                  borderBottomRightRadius: 9,
+                  backgroundColor: "#F3EFFE",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Text
+                  style={{ fontWeight: "bold", color: "#5D3EBD", fontSize: 12 }}
+                >
+                  <Text>{"\u20BA"}</Text>
+                  24,00
+                </Text>
+              </View>
+            </TouchableOpacity>
+          ),
         }}
       />
       <Stack.Screen
@@ -79,6 +124,34 @@ const MyStack = ({ navigation, route }) => {
           headerRight: () => (
             <TouchableOpacity style={{ paddingRight: 12 }}>
               <AntDesign name="heart" size={24} color="#fff" />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+
+      <Stack.Screen
+        name="CartScreen"
+        component={CartScreen}
+        options={{
+          headerTintColor: "#fff",
+          headerBackTitleVisible: false,
+          headerStyle: { backgroundColor: "#5C3EBC" },
+          headerTitle: () => (
+            <Text style={{ fontWeight: "bold", fontSize: 15, color: "#fff" }}>
+              Sepetim
+            </Text>
+          ),
+          headerLeft: () => (
+            <TouchableOpacity
+              style={{ paddingLeft: 6 }}
+              onPress={() => navigation.goBack()}
+            >
+              <AntDesign name="close" size={25} color="#fff" />
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <TouchableOpacity style={{ paddingRight: 6 }}>
+              <Feather name="trash-2" size={24} color="#fff" />
             </TouchableOpacity>
           ),
         }}
---
+++ src/screens/CartScreen/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./styles";
+import { Text, View } from "react-native";
+
+const index = () => {
+  return (
+    <View>
+      <Text>Cart</Text>
+    </View>
+  );
+};
+
+export default index;
