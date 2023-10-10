624391fc46e12da8ae32449bd719a6df97c3f303
yusuf <yusuffdemirci10@gmail.com>
2023-10-10 22:44:58 +0300

    redux installed

--- App.tsx
+++ App.tsx
@@ -3,13 +3,18 @@ import { NavigationContainer } from "@react-navigation/native";
 import RootNavitator from "./src/navigators/RootNavitator";
 import "react-native-gesture-handler";
 import { LogBox } from "react-native";
+import store from "./src/redux/store";
+import { Provider } from "react-redux";
+
 LogBox.ignoreAllLogs(); //Ignore all log notifications
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <RootNavitator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <RootNavitator />
+      </NavigationContainer>
+    </Provider>
   );
 }
 
